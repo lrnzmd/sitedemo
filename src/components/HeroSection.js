e6911cf6425606951ac580d1708a337527b91da7
lorenzo amodeo <lrnzmd@gmail.com>
2020-09-24 16:21:41 +0200

    sistemato css

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,11 +6,11 @@ import '../App.css'
 function Herosection() {
     return (
         <div className="hero-container">
-            <video src="/videod/video-2.mp4" autoplay loop muted/>
+            <video src="/videos/video-2.mp4" autoPlay loop muted/>
             <h1>ADVETURES AWAIT</h1>
             <p>What are you waiting for?</p>
             <div className="hero-btns">
-                <Button className="btn" buttonStyle='btn--outline' buttonStyle="btn-large">
+                <Button className="btn trasparent" buttonStyle='btn--outline' buttonStyle="btn-large" >
                     GET STARTED
                 </Button>
                 <Button className="btn" buttonStyle='btn--primary' buttonStyle="btn-large">
